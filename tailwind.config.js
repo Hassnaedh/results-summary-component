2615ea5badae5be0f04f524b949523ced3b99cf8
Hassnaedh <dehbihassnae@gmail.com>
2024-02-18 21:39:31 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light-red': '#ff5757',
-        'orangey-yellow': '#ffb01f',
-        'green-teal': '#00bd91',
-        'cobalt-blue': '#1125d4',
+        'light-red': {
+          normal: 'hsl(0, 100%, 67%)',
+          light: 'hsla(0, 100%, 67%, .3)',
+        },
+        'orangey-yellow': {
+          normal: 'hsl(39, 100%, 56%)',
+          light: 'hsla(39, 100%, 56%, .2)',
+        },
+        'green-teal': {
+          normal: 'hsl(166, 100%, 37%)',
+          light: 'hsla(166, 100%, 37%, .2)',
+        },
+        'cobalt-blue': {
+          normal: 'hsl(234, 85%, 45%)',
+          light: 'hsla(234, 85%, 45%,.2)',
+        },
         'light-slate-blue': '#7857ff',
         'light-royal-blue': '#2e2be9',
-        'violet-blue': '#4E21CA',
-        'persian-blue': '#2421CA',
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'persian-blue': 'hsla(241, 72%, 46%, 0)',
         'White': '#ffffff',
         'pale-blue': '#ebf1ff',
         'light-lavender': '#c8c7ff',
