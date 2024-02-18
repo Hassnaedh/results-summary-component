5a90b28ceb5998d5e3352a45e5c8ce62bb623159
Hassnaedh <dehbihassnae@gmail.com>
2024-02-18 19:43:22 +0100

    install tailwind css and add some colors to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-red': '#ff5757',
+        'orangey-yellow': '#ffb01f',
+        'green-teal': '#00bd91',
+        'cobalt-blue': '#1125d4',
+        'light-slate-blue': '#7857ff',
+        'light-royal-blue': '#2e2be9',
+        'violet-blue': '#4E21CA',
+        'persian-blue': '#2421CA',
+        'White': '#ffffff',
+        'pale-blue': '#ebf1ff',
+        'light-lavender': '#c8c7ff',
+        'dark-gray-blue': '#303b5a',
+      }
+    },
+  },
+  plugins: [],
+}
+
